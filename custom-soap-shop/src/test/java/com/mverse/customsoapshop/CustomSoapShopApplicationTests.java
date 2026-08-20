9f0c11f10d4d8a867ded816f97eb1c4462246957
uzunovnikolay <nikolay.uzunov@brainstarstech.com>
2026-08-20 13:15:48 +0300

    Configure database environment and CI tests

--- custom-soap-shop/src/test/java/com/mverse/customsoapshop/CustomSoapShopApplicationTests.java
+++ custom-soap-shop/src/test/java/com/mverse/customsoapshop/CustomSoapShopApplicationTests.java
@@ -2,12 +2,21 @@ package com.mverse.customsoapshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest
+@Testcontainers
 class CustomSoapShopApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer postgres =
+	new PostgreSQLContainer("postgres:16");
 
 	@Test
 	void contextLoads() {
 	}
-
 }
