a868b2f777e5b76d4cb7df4f748f8d10f032021f
uzunovnikolay <nikolay.uzunov@brainstarstech.com>
2026-08-21 11:48:06 +0300

    Fix CORS configuration

--- custom-soap-shop/src/main/java/com/mverse/customsoapshop/config/WebConfig.java
+++ custom-soap-shop/src/main/java/com/mverse/customsoapshop/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.mverse.customsoapshop.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${app.cors.allowed-origin}")
+    private String allowedOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
           .allowedOrigins(
             "http://localhost:3000",
-            "http://127.0.0.1:3000"
+            "http://127.0.0.1:3000",
+            allowedOrigin
           )
           .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
           .allowedHeaders("*");
