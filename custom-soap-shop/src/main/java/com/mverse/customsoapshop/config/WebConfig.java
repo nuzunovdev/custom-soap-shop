18b98753a33a2fca557627e90224f25cbb58120f
uzunovnikolay <nikolay.uzunov@brainstarstech.com>
2026-08-22 15:31:21 +0300

    Add health endpoint exposure

--- custom-soap-shop/src/main/java/com/mverse/customsoapshop/config/WebConfig.java
+++ custom-soap-shop/src/main/java/com/mverse/customsoapshop/config/WebConfig.java
@@ -8,17 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${app.cors.allowed-origin}")
-    private String allowedOrigin;
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
           .allowedOrigins(
             "http://localhost:3000",
-            "http://127.0.0.1:3000",
-            allowedOrigin
+            "http://127.0.0.1:3000"
           )
+          .allowedOrigins(allowedOrigins)
           .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
           .allowedHeaders("*");
     }
